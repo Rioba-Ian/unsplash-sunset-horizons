791ea80af67a8e5971335419ca9a18e017042e05
Rioba-Ian <riobafelix@gmail.com>
2024-05-31 18:46:10 +0300

    add custom image loader for next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    loader: "custom",
+    path: "", // you can set a base path if needed
+    loaderFile: "./src/app/_components/ImgContainer.tsx", // path to your custom loader file
+  },
+};
 
 export default nextConfig;
