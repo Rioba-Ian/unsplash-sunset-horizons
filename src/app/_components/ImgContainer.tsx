f80aa63f6c38884f2ff9991312930fb58e496148
Rioba-Ian <riobafelix@gmail.com>
2024-07-24 12:53:51 +0300

    include a delete modal with delete and cancel

--- src/app/_components/ImgContainer.tsx
+++ src/app/_components/ImgContainer.tsx
@@ -1,6 +1,16 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 // import customLoader from "@/lib/imageloader";
 import Image from "next/image";
 import Link from "next/link";
@@ -26,14 +36,12 @@ export default function ImgContainer({
   displayDeleteButton,
 }: ImgContainerProps) {
   const widthHeightRatio = height / width;
-
   const galleryHeight = Math.ceil(360 * widthHeightRatio);
-
   const photoSpans = Math.ceil(galleryHeight / 10) + 1;
 
   return (
     <div
-      className="relative w-[360px] justify-self-center"
+      className="group relative w-[360px] justify-self-center"
       style={{ gridRow: `span ${photoSpans}` }}
     >
       <Link
@@ -41,7 +49,7 @@ export default function ImgContainer({
         target="_blank"
         className="grid place-content-center"
       >
-        <div className="group overflow-hidden rounded-xl">
+        <div className="overflow-hidden rounded-xl">
           <Image
             src={image_url}
             alt={title}
@@ -55,16 +63,39 @@ export default function ImgContainer({
           <p className="invisible absolute inset-2  text-lg text-white group-hover:visible">
             {title}
           </p>
-          {displayDeleteButton && (
+        </div>
+      </Link>
+      {displayDeleteButton && (
+        <Dialog>
+          <DialogTrigger>
             <Button
               variant={"ghost"}
               className="border-1 invisible absolute right-2 top-2 rounded-2xl border-slate-200 text-red-500 group-hover:visible"
             >
               Delete
             </Button>
-          )}
-        </div>
-      </Link>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Delete Photo</DialogTitle>
+              <DialogDescription className="my-4 flex flex-col gap-4">
+                Are you sure you want to delete this photo?
+              </DialogDescription>
+
+              <DialogFooter>
+                <div className="ml-auto mt-4 flex gap-2 self-end">
+                  <Button variant={"destructive"}>Yes, Delete</Button>
+                  <DialogClose asChild>
+                    <Button className="" variant={"secondary"}>
+                      Cancel
+                    </Button>
+                  </DialogClose>
+                </div>
+              </DialogFooter>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+      )}
     </div>
   );
 }
